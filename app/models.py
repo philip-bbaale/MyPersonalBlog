4fe99049b0e90317fd764913a752c816048fb679
Pip Bbaale <namugera.philip@gmail.com>
2020-05-09 11:09:08 +0300

    models.py User

--- app/models.py
+++ app/models.py
@@ -13,7 +13,6 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-        <--->
 
 class Post(db.Model):
     __tablename__='post'
