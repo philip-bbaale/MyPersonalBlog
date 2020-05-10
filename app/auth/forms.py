b344f88ec8e316c5514a8a275bf08f5dc311b380
Pip Bbaale <namugera.philip@gmail.com>
2020-05-10 09:18:22 +0300

    updatting auth forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -30,4 +30,3 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    <!!!!!>
